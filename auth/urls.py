19231d072056283fd315d058e8080692afb5a55f
souravbiswassanto <sourav.cse4.bu@gmail.com>
2023-02-27 12:22:16 +0600

    Updated code

--- auth/urls.py
+++ auth/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
     path('verify_otp/', views.verify_otp, name = "verify_otp"),
+    path('resend_otp/', views.resend_otp, name = 'resend_otp'),
 ]
